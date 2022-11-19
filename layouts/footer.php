8ecf2241106559ad613a2f9e9fb58b0f4bb3f546
Joel lizarazo <80435721+Joellizarazo1151874@users.noreply.github.com>
2022-11-19 16:13:23 -0500

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,140 @@
+<div id="pie2" class="footer-v1 off-container">
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <!-- About -->
+                <div class="col-md-3 col-sm-4 md-margin-bottom-40">
+                    <div class="footer-main">
+                        <a href="index.html"><img id="logo-footer" class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/template/footer/logoufpsvertical.png" alt="Logo Pie de Página UFPS"></a>
+                    </div><br>
+                    <div class="footer-main-colombia">
+                        <a href="https://www.colombia.co" target="_blank"><img id="logo-colombia" class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/seccion/cf7c7322fcc9e80a498c8e0cafc9e5c5.png" alt="Marca País Colombia"></a>
+                    </div><br>
+                    <div class="footer-main-gobierno">
+                        <a href="https://www.gov.co" target="_blank"><img id="logo-gobierno" class="img-responsive" src="https://ww2.ufps.edu.co/public/imagenes/seccion/0c7af37828c031da66297184f075cab3.png" alt="Gobierno de Colombia"></a>
+                    </div>
+                </div>
+                <!--/col-md-3-->
+                <!-- End About -->
+                <!-- Latest -->
+                <div class="col-md-3 col-sm-5 md-margin-bottom-40">
+                    <div class="posts">
+                        <div class="headline" style="border-bottom: #272727;">
+                            <h2>Portales Institucionales</h2>
+                        </div>
+                        <ul class="list-unstyled latest-list">
+                            <li>
+                                <a href="https://divisist2.ufps.edu.co/">Divisist</a>
+                            </li>
+                            <li><a href="https://acceso.ufps.edu.co">Pagos de Egresados y Externos</a></li>
+
+                            <li>
+                                <a href="http://dptosist.ufps.edu.co/piagev1/servlet/piagev">Piagev</a>
+                            </li>
+                            <li>
+                                <a href="/universidad/atencion_ciudadano">PDQRS</a>
+                            </li>
+                            <li>
+                                <a href="http://ugad.ufps.edu.co:8084/datarsoft001/home.ufps">DatarSoft</a>
+                            </li>
+                            <li>
+                                <a href="http://nomina.ufps.edu.co:9191/nominaufps">Sistema de Nómina</a>
+                            </li>
+                            <li>
+                                <a href="http://www.ufps.edu.co/ufps/cread/Presentacion.php">DISERACA</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--/col-md-3-->
+                <!-- End Latest -->
+                <!-- Link List -->
+                <div class="col-md-3 col-sm-5  md-margin-bottom-40">
+                    <div class="headline" style="border-bottom: #272727;">
+                        <h2>Enlaces de Interés</h2>
+                    </div>
+                    <ul class="list-unstyled latest-list">
+                        <li><a href="https://ww2.ufps.edu.co/universidad/planeacion/655" style="text-transform: none;">Plan Anticorrupción</a></li>
+                        <li><a href="https://ww2.ufps.edu.co/universidad/seleccion" style="text-transform: none;">Proceso de selección</a></li>
+                        <li><a href="https://ww2.ufps.edu.co/universidad/contratacion/1122" style="text-transform: none;">Contratación</a></li>
+                        <li><a href="https://ww2.ufps.edu.co/informacion/proceso_democratico_2022" style="text-transform: none;">Proceso democrático</a></li>
+                        <li><a href="https://ww2.ufps.edu.co/vicerrectoria/vicerrectoria-administrativa/527" style="text-transform: none;">Derechos pecuniarios </a></li>
+                        <li><a href="https://mail.google.com/a/ufps.edu.co/" style="text-transform: none;" target="_blank">Correo Electrónico Institucional</a></li>
+                        <li><a href="https://ww2.ufps.edu.co/universidad/consultorio-juridico/1156" style="text-transform: none;">Consultorio Jurídico </a></li>
+                    </ul>
+                </div>
+                <!--/col-md-3-->
+                <!-- End Link List -->
+                <!-- Address -->
+                <div class="col-md-3 col-sm-5  map-img md-margin-bottom-40">
+                    <div class="headline" style="border-bottom: #272727;">
+                        <h2>Contactos</h2>
+                    </div>
+                    <address class="md-margin-bottom-40">
+                        Avenida Gran Colombia No. 12E-96 Barrio Colsag, <br>
+                        San José de Cúcuta - Colombia <br>
+                        Teléfono (057)(7) 5776655 <br> <br>
+
+
+                        Solicitudes y correspondencia<br>
+                        Unidad de Gestión Documental<br>
+                        <a href="mailto:ugad@ufps.edu.co" class="">ugad@ufps.edu.co </a><br><br>
+
+                        Uso único y exclusivo para notificaciones judiciales:<br>
+                        <a href="mailto:notificacionesjudiciales@ufps.edu.co" class="">notificacionesjudiciales@ufps.edu.co</a><br><br>
+
+                    </address>
+                </div>
+                <!--/col-md-3-->
+                <!-- End Address -->
+            </div>
+        </div>
+    </div>
+    <!--/footer-->
+
+    <div class="copyright">
+        <div class="container1">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>
+                        2022 © All Rights Reserved.
+                        Desarrollado por: Joellizarazo <a href="mailto:endersonjoellg@ufps.edu.co"> - División de Sistemas</a>
+                    </p>
+                </div>
+
+                <!-- Social Links -->
+                <div class="col-md-4">
+                    <ul class="list-inline dark-social pull-right space-bottom-0" style="margin-top: 10px;">
+                        <li>
+                            <a data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Facebook" href="https://www.facebook.com/UFPS-C%C3%BAcuta-553833261409690" target="_blank">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Twitter" href="https://twitter.com/UFPSCUCUTA" target="_blank">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Instagram" href="https://www.instagram.com/ufpscucuta/" target="_blank">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Youtube" href="https://www.youtube.com/channel/UCgPz-qqaAk4lbHfr0XH3k2g" target="_blank">
+                                <i class="fa fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-placement="top" data-toggle="tooltip" class="tooltips" data-original-title="Correo" href="mailto:oficinadeprensa@ufps.edu.co" target="_blank">
+                                <i class="fa fa-envelope-o"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- End Social Links -->
+            </div>
+        </div>
+    </div>
+    <!--/copyright-->
+</div>
---
+++ layouts/header.php
@@ -0,0 +1,45 @@
+<header class="section page-header">
+        <!--RD Navbar-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="170px" data-xl-stick-up-offset="170px" data-xxl-stick-up-offset="170px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+            <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+            <div class="rd-navbar-aside-outer">
+              <div class="rd-navbar-aside">
+                <!--RD Navbar Panel-->
+                <div class="rd-navbar-panel">
+                  <!--RD Navbar Toggle-->
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                  <!--RD Navbar Brand-->
+                  <div class="rd-navbar-brand">
+                    <!--Brand--><a class="brand" href="index.php"><img class="brand-logo-dark img-fluid" src="images/logo2.png" alt="" width="180" height="25"/></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="rd-navbar-main-outer">
+              <div class="rd-navbar-main">
+                <div class="rd-navbar-nav-wrap">
+                  <ul class="rd-navbar-nav">
+                    <li class="rd-nav-item active"><a class="rd-nav-link" href="index.php">INICIO</a>
+                    </li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="#tipo_solicitud">PQRS</a>
+                      <ul class="rd-menu rd-navbar-dropdown">
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#tipo_solicitud">Peticiones</a>
+                        </li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#tipo_solicitud">Denuncias</a>
+                        </li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#tipo_solicitud">Quejas</a>
+                        </li>
+                        <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#tipo_solicitud">Reclamos</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="login.php">LOGIN</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
