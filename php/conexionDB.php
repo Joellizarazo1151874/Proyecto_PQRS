3374efd576ffdd2d6fd68bb92781b1d811156668
Camilo Daza <48736699+Camilodaza@users.noreply.github.com>
2022-12-01 06:45:50 -0500

    cambio de db local a deploy
    modificacion de los datos de conexion de la base de datos local al deploy de railway

---
+++ php/conexionDB.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$db-mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+?>
